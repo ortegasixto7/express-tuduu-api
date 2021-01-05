7d303488d1a1aa2d246a3050f0fea9ae86fd532a
Sixto Ortega <ortegasixto7@gmail.com>
2021-01-05 11:04:18 -0400

    Config app

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import routes from './routes/index.js';
+import { connectDatabase } from './config/database.js';
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+connectDatabase(process.env.MONGODB_URL);
+
+// router
+app.use(routes);
+app.listen(process.env.API_PORT, () => {
+  console.log(`[API] listening on port ${process.env.API_PORT}`);
+});
